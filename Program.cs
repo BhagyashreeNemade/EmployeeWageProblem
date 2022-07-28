afc720bb0896f88d87fd47ed59043672ef618d84
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-07-28 16:47:55 +0530

    added uc1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            CheckEmployee.CheckEmployeePresentOrNot();
         }
     }
 }
